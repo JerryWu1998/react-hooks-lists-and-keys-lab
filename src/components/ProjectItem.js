daab72f5780a87bebedea5f3116dfa5e98c28ec5
Yu Wu <yw3748@columbia.edu>
2023-06-16 00:35:02 -0400

    lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const result = technologies.map((tech) => {
+    return (
+      <span key={tech}>{tech}</span>
+    )
+  })
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {result}
       </div>
     </div>
   );
